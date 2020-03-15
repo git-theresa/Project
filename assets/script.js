12ce7cd3bf9479edae6d26cd881eace720ddc15a
Theresa E <t.eatherly@gmail.com>
2020-03-14 21:00:56 -0500

    basic html, .js, css pages made. Pseudo-code on .js

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+//DEAD CODED SOCIETY PSUEDO-CODED
+// what our application does?
+// who is our application for?
+// first step to create? 
+// --- we know we need the user to "input" an artist's name, then click a "button"
+// API 1. - Music Database
+// API 2. - Associated Press
+
+
+// Establish API Key Variables
+// URL Base Name
+// URL Base Name + API Key
+// What components do we need variables for
+//  1. Artist Object= Genre, year started, number one hit (top 5 hits)
+//  2. Articles appear on page when? after artist's stats? do we click a "see more button"
