187e74e4ae51d5de4bef6fc19da815206b0c6580
peacefrog <gregroyclark@gmail.com>
2020-03-21 07:54:08 -0500

    adds name to comment, adds .val & .trim to queryParams

--- assets/script.js
+++ assets/script.js
@@ -16,12 +16,16 @@
 
 //tasteDive api key: 359773-qJams-F4U551BF
 
-//concept borrowed from wk 16, activity 6
+//concept borrowed from wk 16, activity 6 (greg)
 function buildQueryURL() {
 
     var queryURL = "https://tastedive.com/music/like.json?";
 
     var queryParams = {"api-key": "359773-qJams-F4U551BF"};
+
+    queryParams.q = $("#search-term")
+    .val()
+    .trim();
 
 };
 
