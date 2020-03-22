1ae30869551016754fd5ba8e1a7f14e2ccc8fcc3
Theresa E <t.eatherly@gmail.com>
2020-03-21 20:07:16 -0500

    delete pseudocode from .js

--- assets/script.js
+++ assets/script.js
@@ -1,41 +1,46 @@
+// Last FM URL + Base + API Keys ONLY
+var fmURLBase =
+    'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=Cher&api_key=bfab0ca7754766e291154f9b56c5cf7b&format=json';
+var fmURLTag = " /2.0/?method=tag.getinfo&tag=disco&api_key=YOUR_API_KEY&format=json"
+var fmAPIKey = 'bfab0ca7754766e291154f9b56c5cf7b';
+var fmURL =
+	'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=' +
+	userInput +
+	'&api_key=' +
+	fmAPIKey +
+    '&format=json';
 
-//DEAD CODED SOCIETY PSEUDO-CODED
-// what our application does?
-// who is our application for?
-// first step to create? 
-// --- we know we need the user to "input" an artist's name, then click a "button"
-// API 1. - Last FM
-// API 2. - Thrive
-
-
-// Establish API Key Variables
-// URL Base Name
-// URL Base Name + API Key
-// What components do we need variables for
-//  1. Artist Object= Genre, year started, number one hit (top 5 hits)
-//  2. Articles appear on page when? after artist's stats? do we click a "see more button"
-
- //d7bdd05829c75c43b18aac4ec6f2c45871a06f85
- var userInput = "";
-var fmURLBase = "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=Cher&api_key=bfab0ca7754766e291154f9b56c5cf7b&format=json"
-var fmAPIKey = "bfab0ca7754766e291154f9b56c5cf7b"
-var fmURL = "http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=" + "&q="  + userInput + "api_key=" + fmAPIKey;
 console.log(fmURL);
-
- function searchArtist(){
-     console.log(searchArtist);
-     $.ajax({url: fmURL + userInput, method: "GET"})
-     .then(function(response){
+// Other Global Variables for all functions
+var userInput = '';
 
 
-       console.log(response);
-    //$("#userInput").empty();
+// Last FM API call:
+function searchArtist() {
+	console.log(searchArtist);
+	$.ajax({ url: fmURL, method: 'GET' }).then(function(response) {
+		// $('#userInput').empty();
+        // $('#artist').empty();
+        console.log(response);
+		$('#artist').text(response.artist[0].artist[0]);
+		console.log(response.artist.artist[0].artist.name);
+		$('#listen').text(response.main.artist.url);
+        $('#bio').text(response.main.tags.tag.bio);
+         //$("#userInput").empty();
     //$("#artist").empty();
 
     //$("#artist").text(response.main.artist.name);
     //console.log(response.main.artist.name);
     //$("#listen").text(response.main.artist.url);
     //$("#bio").text(response.main.tags.tag.bio);
+
+
+    // end .then(function(response)...Do not remove "  }):  "
+	});
+}
+
+    
+   
 
 
 //tasteDive api key: 359773-qJams-F4U551BF
@@ -75,39 +80,8 @@ console.log(fmURL);
 //     console.log(response);
 // });
 
-var fmURLBase =
-    'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=Cher&api_key=bfab0ca7754766e291154f9b56c5cf7b&format=json';
-var fmURLTag = " /2.0/?method=tag.getinfo&tag=disco&api_key=YOUR_API_KEY&format=json"
-var fmAPIKey = 'bfab0ca7754766e291154f9b56c5cf7b';
-var fmURL =
-	'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=' +
-	userInput +
-	'&api_key=' +
-	fmAPIKey +
-    '&format=json';
-
-console.log(fmURL);
-
-var userInput = '';
 
 
-function searchArtist() {
-	console.log(searchArtist);
-	$.ajax({ url: fmURL, method: 'GET' }).then(function(response) {
-		// $('#userInput').empty();
-        // $('#artist').empty();
-        console.log(response);
-		$('#artist').text(response.artist[0].artist[0]);
-		console.log(response.artist.artist[0].artist.name);
-		$('#listen').text(response.main.artist.url);
-        $('#bio').text(response.main.tags.tag.bio);
-        
-                // end .then(function(response)...Do not remove "  }):  "
-	});
-}
-searchArtist();
-// end .then(function(response)
-// end function searchArtist
 
 // document.ready function(){
 $('#searchBtn').on('click', function(event) {
@@ -124,4 +98,3 @@ $('#searchBtn').on('click', function(event) {
 
 
 
-//  1762a23ec065abade2b42085b6b5f2e60f7118d8
