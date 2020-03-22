012e02f6546538cc9d19ad798eecd771bae9eae6
peacefrog <gregroyclark@gmail.com>
2020-03-22 10:21:12 -0500

    creates tasteDive test file, moves greg blocks over. Erases merge conflict bar from script.js

--- assets/script.js
+++ assets/script.js
@@ -13,44 +13,6 @@
 //  1. Artist Object= Genre, year started, number one hit (top 5 hits)
 //  2. Articles appear on page when? after artist's stats? do we click a "see more button"
 
-//tasteDive api key: 359773-qJams-F4U551BF
-
-//concept borrowed from wk 16, activity 6 (greg)
-// function buildQueryURL() {
-
-//     var queryURL = "https://tastedive.com/music/like.json?";
-
-//     var queryParams = {"api-key": "359773-qJams-F4U551BF"};
-
-//     queryParams.q = $("#search-term")
-//     .val()
-//     .trim();
-
-//     console.log("---------------\nURL: " + queryURL + "\n---------------");
-//     console.log(queryURL + $.param(queryParams));
-//     return queryURL + $.param(queryParams);
-// };
-
-
-// $.ajax({
-//     url: queryURL,
-//     method: "GET"
-// })
-
-// .then(function (response) {
-//     console.log(response);
-// });
-
-// $.ajax({
-//     url: queryURL,
-//     method: "GET"
-// })
-
-// .then(function (response) {
-//     console.log(response);
-// });
-//=======
-
 var fmURLBase =
 	'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=Cher&api_key=bfab0ca7754766e291154f9b56c5cf7b&format=json';
 var fmAPIKey = 'bfab0ca7754766e291154f9b56c5cf7b';
@@ -82,7 +44,7 @@ $('#searchBtn').on('click', function(event) {
 	event.preventDefault();
 	//var userInput = $("#searchTerm").val().trim();
 
-	run();
+	searchArtist()
 });
 
 // document.ready end
