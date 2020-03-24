e817ea315cef8460fa30336dfe84f37cadc5da0d
Theresa E <t.eatherly@gmail.com>
2020-03-24 10:33:16 -0500

    functions commented and tabbed for better review. Missing syntax corrected

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,13 @@
 // Last FM URL + Base + API Keys ONLY
-var fmURLBase =
-	'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=Cher&api_key=bfab0ca7754766e291154f9b56c5cf7b&format=json';
-var fmURLTag =
-	' /2.0/?method=tag.getinfo&tag=disco&api_key=YOUR_API_KEY&format=json';
+var fmURLBase = 'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=Cher&api_key=bfab0ca7754766e291154f9b56c5cf7b&format=json';
 var fmAPIKey = 'bfab0ca7754766e291154f9b56c5cf7b';
 
 // Last FM API call:
 function searchArtist(artist) {
-	var fmURL =
-		'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=' +
-		artist +
-		'&api_key=' +
-		fmAPIKey +
-		'&format=json';
+	var fmURL = 'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=' + artist + '&api_key=' + fmAPIKey + '&format=json';
 	console.log(fmURL);
-	$.ajax({ url: fmURL, method: 'GET' }).then(function(response) {
+
+		$.ajax({ url: fmURL, method: 'GET' }).then(function(response) {
 		console.log(response);
 
 		// EMPTY DIVS BEFORE NEW SEARCH ENTERED:
@@ -23,6 +16,7 @@ function searchArtist(artist) {
 		$('artistName').empty();
 		$('#listen').empty();
 		$('#bio').empty();
+
 		//   BEGIN RESPONSES FROM AJAX CALL
 		$('#artist').text(response.artist.name);
 		console.log(response.artist.name);
@@ -35,38 +29,39 @@ function searchArtist(artist) {
 		// console.log(response.artist.bio.summary);
 
 
-// SIMILAR ARTIST CONTAINER
-$('#similarArtist').empty();
+			// SIMILAR ARTIST CONTAINER
+			$('#similarArtist').empty();
 
-	for (var i = 0; i < 3; i++) {
-	var similarArtistCard = $("<div class='card'>");
-	var similarArtist = $('<div>');
-	$('#similarArtist').empty();
-	$('#similarArtist').text(response.artist.similar.artist[i]);
-	console.log(response.artist.similar.artist[0]);
-	console.log(response.artist.similar.artist[1]);
-	console.log(response.artist.similar.artist[2]);
-	$('#similarArtist').append(similarArtistCard);
+			for (var i = 0; i < 3; i++) {
+			var similarArtistCard = $("<div class='card'>");
+			var similarArtist = $('<div>');
+			$('#similarArtist').empty();
+			$('#similarArtist').text(response.artist.similar.artist[i]);
+			console.log(response.artist.similar.artist[0]);
+			console.log(response.artist.similar.artist[1]);
+			console.log(response.artist.similar.artist[2]);
+			$('#similarArtist').append(similarArtistCard);
 	
 
-	// //END FOR LOOP AJAX
-	}
+			// //END FOR LOOP AJAX - DO NOT REMOVE "}"
+			}
 			
 	});
 	// end .then(function(response)...Do not remove "  }):  "
 }
-// END MAIN FUNCTION
+// END MAIN FUNCTION - DO NOT REMOVE ABOVE "}"
 
 //document.ready function(){
 $('#searchBtn').on('click', function(event) {
-	event.preventDefault();
-	var userInput = $('#userInput')
-		.val()
-		.trim();
-
-	searchArtist(userInput);
+event.preventDefault();
+var userInput = $('#userInput').val().trim();
+searchArtist(userInput);
 });
 
+
+
+
+// ----------------------------New API --------------------- THIS NEEDS A SEPARATE PAGE TO MAKE IT FUNCTION FIRST
 //sidenav
 //const sideNav = document.querySelector('.sidenav');
 //M.Sidenav.init(sideNav, {});
