7f9e156559179c1c0e0d7d8a7ccce48e6ecdb36c
peacefrog <gregroyclark@gmail.com>
2020-03-24 13:23:17 -0500

    adds discogs test file, begins api experiment

---
+++ assets/discogs.js
@@ -0,0 +1,42 @@
+// ----------------------------New API --------------------- THIS NEEDS A SEPARATE PAGE TO MAKE IT FUNCTION FIRST
+//sidenav
+//const sideNav = document.querySelector('.sidenav');
+//M.Sidenav.init(sideNav, {});
+
+// searchArtist();
+//initialize image
+//$(document).ready(function() {
+//	$('.parallax').parallax();
+//});
+
+//run(searchArtist);
+// document.ready end
+// 
+// 
+	//discogs URL with Kristin's keys
+			//'https://api.discogs.com/database/search?q=Whitney+Houston&key=nvRXMBDnKmKOoUevQtuL&secret=hJCijCBPkQSmbnplJHWgvxreRNvhKVSN'
+
+			// var key = 'nvRXMBDnKmKOoUevQtuL';
+			// var secret = 'hJCijCBPkQSmbnplJHWgvxreRNvhKVSN';
+			// function artistImage(coverImage) {
+			// 	var discogsURL =
+			// 		'https://api.discogs.com/database/search?q=' +
+			// 		coverImage +
+			// 		'&key=' +
+			// 		key +
+			// 		'&secret=' +
+			// 		secret;
+			// 	console.log(discogsURL);
+
+			// 	$.ajax({ url: discogsURL, method: 'GET' }).then(function(response) {
+			// 		console.log(response);
+
+			// 		var newImage = $('<img>').attr(
+			// 			'src',
+			// 			response.results[0].cover_image
+			// 		);
+			// 		$('#test').append(newImage);
+			// 	});
+			// }
+			// artistImage('cher');
+			// artistImage('madonna');
