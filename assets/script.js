ccc0d02b4a516e6c6fd782b0927e9168ed0322b7
peacefrog <gregroyclark@gmail.com>
2020-03-24 20:30:48 -0500

    2nd api successful, comments all console logs, adds 2nd function to button, minor refactoring, deletesdiscogs & tastedive files

--- assets/script.js
+++ assets/script.js
@@ -5,10 +5,10 @@ var fmAPIKey = 'bfab0ca7754766e291154f9b56c5cf7b';
 // Last FM API call:
 function searchArtist(artist) {
 	var fmURL = 'https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=' + artist + '&api_key=' + fmAPIKey + '&format=json';
-	console.log(fmURL);
+	//console.log(fmURL);
 
 		$.ajax({ url: fmURL, method: 'GET' }).then(function(response) {
-		console.log(response);
+		//console.log(response);
 
 		// EMPTY DIVS BEFORE NEW SEARCH ENTERED:
 		$('#userInput').empty();
@@ -19,11 +19,11 @@ function searchArtist(artist) {
 
 		//   BEGIN RESPONSES FROM AJAX CALL
 		$('#artist').text(response.artist.name);
-		console.log(response.artist.name);
+		//console.log(response.artist.name);
 		$('#artistName').append('<h1>' + response.artist.name + '</h1>' );
 	
 		$('#listen').text(response.artist.url);
-		console.log(response.artist.url);
+		//console.log(response.artist.url);
 		$('#artistName').append(`<a href="${response.artist.url}" target="blank">  ${response.artist.url}  </a>`);
 
 	
@@ -41,13 +41,13 @@ function searchArtist(artist) {
 			$('#similarArtist').empty();
 
 			$('#similarArtist').text(response.artist.similar.artist[i].name );
-			console.log(response.artist.similar.artist[0].name);
-			console.log(response.artist.similar.artist[1].name);
-			console.log(response.artist.similar.artist[2].name);
+			// console.log(response.artist.similar.artist[0].name);
+			// console.log(response.artist.similar.artist[1].name);
+			// console.log(response.artist.similar.artist[2].name);
 
 			var similarArtistURL = $('<div>');
 			$('#similarArtistURL').text(response.artist.similar.artist[i].url);
-			console.log(response.artist.similar.artist[i].url);
+			//console.log(response.artist.similar.artist[i].url);
 			$('#similarArtist').append(`<a href="${response.artist.similar.artist[i].url}" target="blank">  ${response.artist.similar.artist[i].url}  </a>`)
 			
 			
@@ -62,12 +62,15 @@ function searchArtist(artist) {
 			
 	});
 	// end .then(function(response)...Do not remove "  }):  "
+
+
+
 }
 // END MAIN FUNCTION - DO NOT REMOVE ABOVE "}"
 
-var key = 'nvRXMBDnKmKOoUevQtuL';
-var secret = 'hJCijCBPkQSmbnplJHWgvxreRNvhKVSN';
 function artistImage(coverImage) {
+	var key = 'nvRXMBDnKmKOoUevQtuL';
+	var secret = 'hJCijCBPkQSmbnplJHWgvxreRNvhKVSN';
 	var discogsURL =
 		'https://api.discogs.com/database/search?q=' +
 		coverImage +
@@ -76,43 +79,27 @@ function artistImage(coverImage) {
 		'&secret=' +
 		secret;
 	console.log(discogsURL);
+	console.log(coverImage);
 
 	$.ajax({ url: discogsURL, method: 'GET' }).then(function(response) {
 		console.log(response);
+
+
 
 		var newImage = $('<img>').attr(
 			'src',
 			response.results[0].cover_image
 		);
-		$('#test').append(newImage);
+		$('#artistImg').empty();
+		$('#artistImg').append(newImage);
 	});
 }
-artistImage('cher');
-artistImage('madonna');
+
 
 //document.ready function(){
 $('#searchBtn').on('click', function(event) {
 event.preventDefault();
 var userInput = $('#userInput').val().trim();
 searchArtist(userInput);
+artistImage(userInput);
 });
-
-// ----------------------------New API --------------------- 
-//THIS NEEDS A SEPARATE PAGE TO MAKE IT FUNCTION FIRST
-//sidenav
-//const sideNav = document.querySelector('.sidenav');
-//M.Sidenav.init(sideNav, {});
-
-// searchArtist();
-//initialize image
-//$(document).ready(function() {
-//	$('.parallax').parallax();
-//});
-
-//run(searchArtist);
-// document.ready end
-// 
-	//discogs URL with Kristin's keys
-			//'https://api.discogs.com/database/search?q=Whitney+Houston&key=nvRXMBDnKmKOoUevQtuL&secret=hJCijCBPkQSmbnplJHWgvxreRNvhKVSN'
-
-
